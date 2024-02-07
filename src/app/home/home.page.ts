d1a9c05176fe81be4d2b47d2e4889bde9fb7e07a
Mateo-EC <mathiasmogrovilla533@gmail.com>
2024-02-07 14:52:29 -0500

    PAPA

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,65 @@
 import { Component } from '@angular/core';
-
+import { Firestore, doc, setDoc, } from '@angular/fire/firestore';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  ledstate: any;
+  ledStatus: boolean = false;
+  ledStatusL2: boolean = false;
+  ledStatusL3: boolean = false;
+  ledStatusL4: boolean = false;
+  rutadelatabla: any;
+  rutadelatabla4: any;
+  rutadelatabla2: any;
+  rutadelatabla3: any;
+  constructor(private db: Firestore) {
+  }
+  /*
+  async apagar() {
+    this.ledstate = doc(this.db, 'controlLED', 'LED1');
+    await setDoc(this.ledstate, { encender: false });
+  }
+  async encender() {
+    this.ledstate = doc(this.db, 'controlLED', 'LED1');
+    await setDoc(this.ledstate, { encender: true });
+  }
+*/
+  async toogleState() {
+    this.ledStatus = !this.ledStatus
+    this.rutadelatabla = doc(this.db, "controlLed", "LED 1");
+    await setDoc(this.rutadelatabla, { encender: this.ledStatus })
+    console.log(this.ledStatus);
 
-  constructor() {}
+  }
+
+  async toogleStateL2() {
+    this.ledStatusL2 = !this.ledStatusL2
+    this.rutadelatabla2 = doc(this.db, "controlLed", "LED 2");
+    await setDoc(this.rutadelatabla2, { encender: this.ledStatusL2 })
+    console.log(this.ledStatusL2);
+  }
+
+  async toogleStateL3() {
+    this.ledStatusL3 = !this.ledStatusL3
+    this.rutadelatabla3 = doc(this.db, "controlLed", "LED 3");
+    await setDoc(this.rutadelatabla3, { encender: this.ledStatusL3 })
+    console.log(this.ledStatusL3);
+  }
+
+  async toogleStateL4() {
+    this.ledStatusL4 = !this.ledStatusL4
+    this.ledStatusL3 = this.ledStatusL4
+    this.ledStatusL2 = this.ledStatusL4
+    this.ledStatus = this.ledStatusL4
+    this.rutadelatabla4 = doc(this.db, "controlLed", "LED 4");
+    await setDoc(this.rutadelatabla4, { encender: this.ledStatusL4})
+    await setDoc(this.rutadelatabla3, { encender: this.ledStatusL3 })
+    await setDoc(this.rutadelatabla2, { encender: this.ledStatusL2 })
+    await setDoc(this.rutadelatabla, { encender: this.ledStatus })
+    console.log(this.ledStatusL4);
+  }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDiO-O1CHOS3ja7zuRq4bRV7zOrO7SK64c",
+    authDomain: "pm1-emc.firebaseapp.com",
+    projectId: "pm1-emc",
+    storageBucket: "pm1-emc.appspot.com",
+    messagingSenderId: "701486042008",
+    appId: "1:701486042008:web:0992f62a6d3a5d092339ed"
+  }
 };
 
 /*
